9034a0c72100e945b62b19d0fa965152432cf67f
fiufinhou <141530737+fiufinhou@users.noreply.github.com>
2025-02-08 22:59:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # إعدادات بوت تيليجرام
 BOT_TOKEN = "6546516734:AAFHElupAZvxFfwALkbHhA-UOrf_4pJM3ok"
 CHAT_ID = "1722250078"
-
+‏SERVER_URL = "https://tr-zz39.onrender.com"   
 # إعدادات خادم Flask
 HOST = "0.0.0.0"
 PORT = 5000
